902e1d135ba2d971042a28c911ec27fe2c8ff5ad
Bordianu Raul <raul.bordianu@stud.ubbcluj.ro>
2024-10-17 20:11:59 +0300

    Backend

---
+++ Backend/Backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    return jsonify({"message": "Hello world!"})
+
+if __name__ == '__main__':
+    app.run()
