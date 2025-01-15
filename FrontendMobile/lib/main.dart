636991e30e6341116a273fe92278c148a1f65231
Bordianu Raul <raul.bordianu@stud.ubbcluj.ro>
2025-01-16 00:11:06 +0200

    Front-end update

--- FrontendMobile/lib/main.dart
+++ FrontendMobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:frontend_mobile/src/screens/login_screen.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: LoginScreen(),
     theme: ThemeData(primarySwatch: Colors.blue),
   ));
